fa5756c85f93d1198278465fa11c2974ba16192a
JennySabra <114403465+JennySabra@users.noreply.github.com>
2022-09-26 20:07:49 +0300

    Update jenny.py

--- jenny.py
+++ jenny.py
@@ -32,11 +32,11 @@ fig2 = px.bar(df, x="Location", y="Number of Beds", color="Hospital")
 st.plotly_chart(fig2)
 
 st.header("Covid-19 cases")
-df= pd.read_csv("C:\\Users\\student\\Desktop\\covid-vaccination-vs-death_ratio.csv")
+df= pd.read_csv("covid-vaccination-vs-death_ratio.csv")
 fig3= px.scatter(df, x="people_vaccinated", y="New_deaths", animation_frame="date", animation_group="country", color="country", hover_name="country", size="population", size_max= 30, log_x= True)
 st.plotly_chart(fig3)
 
-df= pd.read_csv("C:\\Users\\student\\Desktop\\covid-vaccination-vs-death_ratio.csv")
+df= pd.read_csv("covid-vaccination-vs-death_ratio.csv")
 fig4= px.bar(df, x="country", y="total_vaccinations", color="country",
   animation_frame="date", animation_group="country", range_y=[0,100000000])
 st.plotly_chart(fig4)
